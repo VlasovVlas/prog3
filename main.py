2818d7be83985b9f6eee28f7605d694200140364
VlasovVlas <vlas.vlasov.02@mail.ru>
2019-10-22 02:44:07 +0700

    Initial project.

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ if n1 == 1 or n2 == 1 or n3 == 1:
             n5 = n5 - n2
             n6 = n6 - n3
             a2 = a2 - a1
-        print(n4, n5, n6, a2)
+        print(n4, n5, n6, '|', a2)
 
         while n7 != 0:
             n7 = n7 - n1
             n8 = n8 - n2
             n9 = n9 - n3
             a3 = a3 - a1
-        print(n7, n8, n9, a3)
+        print(n7, n8, n9, '|',a3)
         exit()
 
     elif n2 == 1:
@@ -27,14 +27,14 @@ if n1 == 1 or n2 == 1 or n3 == 1:
             n5 = n5 - n2
             n6 = n6 - n3
             a2 = a2 - a1
-        print(n4, n5, n6, a2)
+        print(n4, n5, n6, '|',a2)
 
         while n8 != 0:
             n7 = n7 - n1
             n8 = n8 - n2
             n9 = n9 - n3
             a3 = a3 - a1
-        print(n7, n8, n9, a3)
+        print(n7, n8, n9, '|',a3)
         exit()
 
     elif n3 == 1:
@@ -43,33 +43,33 @@ if n1 == 1 or n2 == 1 or n3 == 1:
             n5 = n5 - n2
             n6 = n6 - n3
             a2 = a2 - a1
-        print(n4, n5, n6, a2)
+        print(n4, n5, n6, '|',a2)
 
         while n9 != 0:
             n7 = n7 - n1
             n8 = n8 - n2
             n9 = n9 - n3
             a3 = a3 - a1
-        print(n7, n8, n9, a3)
+        print(n7, n8, n9, '|',a3)
         exit()
 
 elif n4 == 1 or n5 == 1 or n6 == 1:
     print()
-    print(n4, n5, n6, a2)
+    print(n4, n5, n6, '|',a2)
     if n4 == 1:
         while n1 != 0:
             n1 = n1 - n4
             n2 = n2 - n5
             n3 = n3 - n6
             a1 = a1 - a2
-        print(n1, n2, n3, a1)
+        print(n1, n2, n3, '|',a1)
 
         while n7 != 0:
             n7 = n7 - n4
             n8 = n8 - n5
             n9 = n9 - n6
             a3 = a3 - a2
-        print(n7, n8, n9, a3)
+        print(n7, n8, n9, '|',a3)
         exit()
 
     elif n5 == 1:
@@ -78,14 +78,14 @@ elif n4 == 1 or n5 == 1 or n6 == 1:
             n2 = n2 - n5
             n3 = n3 - n6
             a1 = a1 - a2
-        print(n1, n2, n3, a1)
+        print(n1, n2, n3, '|',a1)
 
         while n8 != 0:
             n7 = n7 - n4
             n8 = n8 - n5
             n9 = n9 - n6
             a3 = a3 - a2
-        print(n7, n8, n9, a3)
+        print(n7, n8, n9, '|',a3)
         exit()
 
     elif n6 == 1:
@@ -94,33 +94,33 @@ elif n4 == 1 or n5 == 1 or n6 == 1:
             n2 = n2 - n5
             n3 = n3 - n6
             a1 = a1 - a2
-        print(n1, n2, n3, a1)
+        print(n1, n2, n3, '|',a1)
 
         while n9 != 0:
             n7 = n7 - n4
             n8 = n8 - n5
             n9 = n9 - n6
             a3 = a3 - a2
-        print(n7, n8, n9, a3)
+        print(n7, n8, n9, '|',a3)
         exit()
 
 elif n7 == 1 or n8 == 1 or n9 == 1:
     print()
-    print(n7, n8, n9, a3)
+    print(n7, n8, n9, '|',a3)
     if n7 == 1:
         while n1 != 0:
             n1 = n1 - n7
             n2 = n2 - n8
             n3 = n3 - n9
             a1 = a1 - a3
-        print(n1, n2, n3, a1)
+        print(n1, n2, n3, '|',a1)
 
         while n4 != 0:
             n4 = n4 - n7
             n5 = n5 - n8
             n6 = n6 - n9
             a2 = a2 - a3
-        print(n4, n5, n6, a2)
+        print(n4, n5, n6, '|',a2)
         exit()
 
     elif n8 == 1:
@@ -129,14 +129,14 @@ elif n7 == 1 or n8 == 1 or n9 == 1:
             n2 = n2 - n8
             n3 = n3 - n9
             a1 = a1 - a3
-        print(n1, n2, n3, a1)
+        print(n1, n2, n3, '|',a1)
 
         while n5 != 0:
             n4 = n4 - n7
             n5 = n5 - n8
             n6 = n6 - n9
             a2 = a2 - a3
-        print(n4, n5, n6, a2)
+        print(n4, n5, n6, '|',a2)
         exit()
 
     elif n9 == 1:
@@ -145,12 +145,12 @@ elif n7 == 1 or n8 == 1 or n9 == 1:
             n2 = n2 - n8
             n3 = n3 - n9
             a1 = a1 - a3
-        print(n1, n2, n3, a1)
+        print(n1, n2, n3, '|',a1)
 
         while n6 != 0:
             n4 = n4 - n7
             n5 = n5 - n8
             n6 = n6 - n9
             a2 = a2 - a3
-        print(n4, n5, n6, a2)
+        print(n4, n5, n6, '|',a2)
         exit()
