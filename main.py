284848416bfb145b1645f2e7dee202db1f406190
VlasovVlas <vlas.vlasov.02@mail.ru>
2019-10-22 00:02:40 +0700

    Initial project.

---
+++ main.py
@@ -0,0 +1,17 @@
+n1, n2, n3, a1 = map(int, input('').split())
+n4, n5, n6, a2 = map(int, input('').split())
+n7, n8, n9, a3 = map(int, input('').split())
+
+if n1 == 1 or n2 == 1 or n3 == 1 or n1 == -1 or n2 == -1 or n3 == -1:
+    
+
+
+
+
+elif n4 == 1 or n5 == 1 or n6 == 1 or n4 == -1 or n5 == -1 or n6 == -1:
+
+
+
+
+
+elif n7 == 1 or n8 == 1 or n9 == 1 or n7 == -1 or n8 == -1 or n9 == -1:
