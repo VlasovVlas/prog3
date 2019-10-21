08f7b828bd0507b3ee2567920b305b39ecea023c
VlasovVlas <vlas.vlasov.02@mail.ru>
2019-10-22 01:34:13 +0700

    Initial project.

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ if n1 == 1 or n2 == 1 or n3 == 1:
             n5 = n5 - n2
             n6 = n6 - n3
             a2 = a2 - a1
+            print(n4, n5, n6, a2)
+            print()
 
         while n7 != 0:
             n7 = n7 - n1
             n8 = n8 - n2
             n9 = n9 - n3
             a3 = a3 - a1
+            print(n7, n8, n9, a3)
+            print()
 
     elif n2 == 1:
         while n5 != 0:
@@ -22,12 +26,16 @@ if n1 == 1 or n2 == 1 or n3 == 1:
             n5 = n5 - n2
             n6 = n6 - n3
             a2 = a2 - a1
+            print(n4, n5, n6, a2)
+            print()
 
         while n8 != 0:
             n7 = n7 - n1
             n8 = n8 - n2
             n9 = n9 - n3
             a3 = a3 - a1
+            print(n7, n8, n9, a3)
+            print()
 
     elif n3 == 1:
         while n6 != 0:
@@ -35,12 +43,16 @@ if n1 == 1 or n2 == 1 or n3 == 1:
             n5 = n5 - n2
             n6 = n6 - n3
             a2 = a2 - a1
+            print(n4, n5, n6, a2)
+            print()
 
         while n9 != 0:
             n7 = n7 - n1
             n8 = n8 - n2
             n9 = n9 - n3
             a3 = a3 - a1
+            print(n7, n8, n9, a3)
+            print()
 
 elif n4 == 1 or n5 == 1 or n6 == 1:
     if n4 == 1:
@@ -49,12 +61,16 @@ elif n4 == 1 or n5 == 1 or n6 == 1:
             n2 = n2 - n5
             n3 = n3 - n6
             a1 = a1 - a2
+            print(n1, n2, n3, a1)
+            print()
 
         while n7 != 0:
             n7 = n7 - n4
             n8 = n8 - n5
             n9 = n9 - n6
             a3 = a3 - a2
+            print(n7, n8, n9, a3)
+            print()
 
     elif n5 == 1:
         while n2 != 0:
@@ -62,12 +78,16 @@ elif n4 == 1 or n5 == 1 or n6 == 1:
             n2 = n2 - n5
             n3 = n3 - n6
             a1 = a1 - a2
+            print(n1, n2, n3, a1)
+            print()
 
         while n8 != 0:
             n7 = n7 - n4
             n8 = n8 - n5
             n9 = n9 - n6
             a3 = a3 - a2
+            print(n7, n8, n9, a3)
+            print()
 
     elif n6 == 1:
         while n3 != 0:
@@ -75,12 +95,16 @@ elif n4 == 1 or n5 == 1 or n6 == 1:
             n2 = n2 - n5
             n3 = n3 - n6
             a1 = a1 - a2
+            print(n1, n2, n3, a1)
+            print()
 
         while n9 != 0:
             n7 = n7 - n4
             n8 = n8 - n5
             n9 = n9 - n6
             a3 = a3 - a2
+            print(n7, n8, n9, a3)
+            print()
 
 elif n7 == 1 or n8 == 1 or n9 == 1:
     if n7 == 1:
@@ -89,12 +113,16 @@ elif n7 == 1 or n8 == 1 or n9 == 1:
             n2 = n2 - n8
             n3 = n3 - n9
             a1 = a1 - a3
+            print(n1, n2, n3, a1)
+            print()
 
         while n4 != 0:
             n4 = n4 - n7
             n5 = n5 - n8
             n6 = n6 - n9
             a2 = a2 - a3
+            print(n4, n5, n6, a2)
+            print()
 
     elif n8 == 1:
         while n2 != 0:
@@ -102,12 +130,16 @@ elif n7 == 1 or n8 == 1 or n9 == 1:
             n2 = n2 - n8
             n3 = n3 - n9
             a1 = a1 - a3
+            print(n1, n2, n3, a1)
+            print()
 
         while n5 != 0:
             n4 = n4 - n7
             n5 = n5 - n8
             n6 = n6 - n9
             a2 = a2 - a3
+            print(n4, n5, n6, a2)
+            print()
 
     elif n9 == 1:
         while n3 != 0:
@@ -115,9 +147,13 @@ elif n7 == 1 or n8 == 1 or n9 == 1:
             n2 = n2 - n8
             n3 = n3 - n9
             a1 = a1 - a3
+            print(n1, n2, n3, a1)
+            print()
 
         while n6 != 0:
             n4 = n4 - n7
             n5 = n5 - n8
             n6 = n6 - n9
             a2 = a2 - a3
+            print(n4, n5, n6, a2)
+            print()
