9b227c467c039e99c9f90846370a699b62daa1cb
VlasovVlas <vlas.vlasov.02@mail.ru>
2019-10-22 01:17:58 +0700

    Initial project.

--- main.py
+++ main.py
@@ -2,10 +2,122 @@ n1, n2, n3, a1 = map(int, input('').split())
 n4, n5, n6, a2 = map(int, input('').split())
 n7, n8, n9, a3 = map(int, input('').split())
 
-if n1 == 1 or n2 == 1 or n3 == 1 or n1 == -1 or n2 == -1 or n3 == -1:
+if n1 == 1 or n2 == 1 or n3 == 1:
+    if n1 == 1:
+        while n4 != 0:
+            n4 = n4 - n1
+            n5 = n5 - n2
+            n6 = n6 - n3
+            a2 = a2 - a1
 
+        while n7 != 0:
+            n7 = n7 - n1
+            n8 = n8 - n2
+            n9 = n9 - n3
+            a3 = a3 - a1
 
-elif n4 == 1 or n5 == 1 or n6 == 1 or n4 == -1 or n5 == -1 or n6 == -1:
+    elif n2 == 1:
+        while n5 != 0:
+            n4 = n4 - n1
+            n5 = n5 - n2
+            n6 = n6 - n3
+            a2 = a2 - a1
 
+        while n8 != 0:
+            n7 = n7 - n1
+            n8 = n8 - n2
+            n9 = n9 - n3
+            a3 = a3 - a1
 
-elif n7 == 1 or n8 == 1 or n9 == 1 or n7 == -1 or n8 == -1 or n9 == -1:
+    elif n3 == 1:
+        while n6 != 0:
+            n4 = n4 - n1
+            n5 = n5 - n2
+            n6 = n6 - n3
+            a2 = a2 - a1
+
+        while n9 != 0:
+            n7 = n7 - n1
+            n8 = n8 - n2
+            n9 = n9 - n3
+            a3 = a3 - a1
+
+elif n4 == 1 or n5 == 1 or n6 == 1:
+    if n4 == 1:
+        while n1 != 0:
+            n1 = n1 - n4
+            n2 = n2 - n5
+            n3 = n3 - n6
+            a1 = a1 - a2
+
+        while n7 != 0:
+            n7 = n7 - n4
+            n8 = n8 - n5
+            n9 = n9 - n6
+            a3 = a3 - a2
+
+    elif n5 == 1:
+        while n2 != 0:
+            n1 = n1 - n4
+            n2 = n2 - n5
+            n3 = n3 - n6
+            a1 = a1 - a2
+
+        while n8 != 0:
+            n7 = n7 - n4
+            n8 = n8 - n5
+            n9 = n9 - n6
+            a3 = a3 - a2
+
+    elif n6 == 1:
+        while n3 != 0:
+            n1 = n1 - n4
+            n2 = n2 - n5
+            n3 = n3 - n6
+            a1 = a1 - a2
+
+        while n9 != 0:
+            n7 = n7 - n4
+            n8 = n8 - n5
+            n9 = n9 - n6
+            a3 = a3 - a2
+
+elif n7 == 1 or n8 == 1 or n9 == 1:
+    if n7 == 1:
+        while n1 != 0:
+            n1 = n1 - n7
+            n2 = n2 - n8
+            n3 = n3 - n9
+            a1 = a1 - a3
+
+        while n4 != 0:
+            n4 = n4 - n7
+            n5 = n5 - n8
+            n6 = n6 - n9
+            a2 = a2 - a3
+
+    elif n8 == 1:
+        while n2 != 0:
+            n1 = n1 - n7
+            n2 = n2 - n8
+            n3 = n3 - n9
+            a1 = a1 - a3
+
+        while n5 != 0:
+            n4 = n4 - n7
+            n5 = n5 - n8
+            n6 = n6 - n9
+            a2 = a2 - a3
+
+    elif n9 == 1:
+        while n3 != 0:
+            n1 = n1 - n7
+            n2 = n2 - n8
+            n3 = n3 - n9
+            a1 = a1 - a3
+
+        while n6 != 0:
+            n4 = n4 - n7
+            n5 = n5 - n8
+            n6 = n6 - n9
+            a2 = a2 - a3
